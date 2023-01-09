b489c77a539c8351344bb9cff5516ff54f1dc7c8
D-Sketon <2055272094@qq.com>
2023-01-09 18:29:35 +0800

    feat: sidebar基本实现

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,26 @@
+// https://github.com/tangyuxian/hexo-theme-tangyuxian
+const getRealPath = (pathname, desc = false) => {
+    if (!pathname) {
+        pathname = window.location.pathname;
+    }
+    let names = pathname.split("/");
+    if (desc === false) {
+        for (let i = names.length - 1; i >= 0; --i) {
+            let name = names[i].trim();
+            if (name.length > 0 && name !== "/" && name !== "index.html") {
+                return name;
+            }
+        }
+    } else {
+        for (let i = 0; i < names.length; ++i) {
+            let name = names[i].trim();
+            if (name.length > 0 && name !== "/" && name !== "index.html") {
+                return name;
+            }
+        }
+    }
+    return "/";
+};
 (function ($) {
     // Caption
     $('.article-entry').each(function (i) {
@@ -48,4 +71,13 @@
 
         $container.removeClass('mobile-nav-on');
     });
+
+
+    let rootRealPath = getRealPath(window.location.pathname, true);
+    for (let link of $('.sidebar-menu-link-wrap')) {
+        let linkPath = $(link).find("a")[0].getAttribute("href");
+        if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
+            link.className = "sidebar-menu-link-wrap link-active";
+        }
+    }
 })(jQuery);
