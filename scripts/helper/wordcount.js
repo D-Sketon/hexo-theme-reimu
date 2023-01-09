736c6d41924fbed7014241744783953ac30a3889
D-Sketon <2055272094@qq.com>
2023-01-09 21:47:36 +0800

    feat: footer基本实现

--- scripts/helper/wordcount.js
+++ scripts/helper/wordcount.js
@@ -1,4 +1,4 @@
-// https://github.com/willin/hexo-wordcount
+// 改自 https://github.com/willin/hexo-wordcount
 const util = require('hexo-util')
 const stripHTML = util.stripHTML
 
@@ -13,11 +13,22 @@ const countFn = (content) => {
     let len = counter(content)
     return len[0] + len[1]
 }
-
-hexo.extend.helper.register('min2read', (content, {cn = 300, en = 160} = {}) => {
+const changeHourMinuteStr = (str) => {
+    if (str !== "0" && str !== "" && str !== null) {
+        return ((Math.floor(str / 60)).toString().length < 2 ? "0" + (Math.floor(str / 60)).toString() :
+            (Math.floor(str / 60)).toString()) + ":" + ((str % 60).toString().length < 2 ? "0" + (str % 60).toString() : (str % 60).toString())
+    } else {
+        return "";
+    }
+}
+const timeFn = (content, {cn = 300, en = 160} = {}) => {
     let len = counter(content)
     let readingTime = len[0] / cn + len[1] / en
-    return readingTime < 1 ? '1' : parseInt(readingTime, 10)
+    return readingTime < 1 ? 1 : parseInt(readingTime, 10)
+}
+
+hexo.extend.helper.register('min2read', (content, {cn = 300, en = 160} = {}) => {
+    return timeFn(content, {cn, en})
 })
 
 hexo.extend.helper.register('wordcount', content => {
@@ -29,4 +40,10 @@ hexo.extend.helper.register('totalcount', site => {
     let count = 0
     site.posts.forEach(post => count += countFn(post.content))
     return count < 1000 ? count : Math.round(count / 100) / 10 + 'k'
+})
+
+hexo.extend.helper.register('totalmin2read', site => {
+    let readingTime = 0
+    site.posts.forEach(post => readingTime += timeFn(post.content))
+    return changeHourMinuteStr(readingTime.toString())
 })
