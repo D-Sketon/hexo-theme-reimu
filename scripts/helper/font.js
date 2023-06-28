8c2b0737acdc12b6e2416f5fde7e5bd902f00e4e
D-Sketon <2055272094@qq.com>
2023-06-28 23:52:24 +0800

    refactor: 移除本地Ubuntu Mono

---
+++ scripts/helper/font.js
@@ -0,0 +1,24 @@
+const { htmlTag }  = require('hexo-util'
+)
+hexo.extend.helper.register('vendorFont', () => {
+  const fontDisplay = '&display=swap'
+  const fontSubset = '&subset=latin,latin-ext'
+  const fontStyles = ':300,300italic,400,400italic,700,700italic'
+  const fontHost = 'https://fonts.googleapis.com'
+
+  let fontFamilies = ['Mulish', 'Noto Serif SC', 'Ubuntu Mono'].map(item => {
+    return item + fontStyles
+  })
+
+  fontFamilies = fontFamilies.filter(item => item !== '')
+  fontFamilies = [...new Set(fontFamilies)]
+  fontFamilies = fontFamilies.join('|')
+
+  // Merge extra parameters to the final processed font string
+  return fontFamilies
+    ? htmlTag('link', {
+      rel: 'stylesheet',
+      href: `${fontHost}/css?family=${fontFamilies.concat(fontDisplay, fontSubset)}`
+    })
+    : ''
+})
