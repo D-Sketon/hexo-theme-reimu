1a77515a5167ad089954edb4b36845b1243f0261
D-Sketon <2055272094@qq.com>
2023-07-02 13:51:01 +0800

    feat: 黑暗模式

--- source/js/script.js
+++ source/js/script.js
@@ -23,6 +23,40 @@ const getRealPath = (pathname, desc = false) => {
 };
 
 (function ($) {
+  // dark_mode
+  let mode = window.localStorage.getItem('dark_mode')
+  if (mode == null) {
+    const domMode = document.documentElement.getAttribute('data-theme')
+    if (domMode == null) {
+      window.localStorage.setItem('dark_mode', 'false')
+    } else {
+      window.localStorage.setItem('dark_mode', 'true')
+    }
+  } else {
+    if(mode == 'true') {
+      document.documentElement.setAttribute('data-theme', 'dark')
+    } else if (mode == 'false') {
+      document.documentElement.removeAttribute('data-theme')
+    }
+  }
+  mode = window.localStorage.getItem('dark_mode')
+  if(mode == 'true') {
+    $('#sub-nav').append('<a id="nav-sun-btn" class="nav-icon dark-mode-btn"></a>')
+  } else if (mode == 'false') {
+    $('#sub-nav').append('<a id="nav-moon-btn" class="nav-icon dark-mode-btn"></a>')
+  }
+  $('.dark-mode-btn').on('click', function () {
+    const id = $(this).attr('id')
+    if(id == 'nav-sun-btn') { 
+      window.localStorage.setItem('dark_mode', 'false')
+      document.documentElement.removeAttribute('data-theme')
+      $(this).attr("id","nav-moon-btn")
+    } else {
+      window.localStorage.setItem('dark_mode', 'true')
+      document.documentElement.setAttribute('data-theme', 'dark')
+      $(this).attr("id","nav-sun-btn")
+    }
+  })
   // lazysizes
   const imgs = $('.article-entry img');
   imgs.each(function() {
