4dfc4a448a065dc730336c51ffbe61ef948ddf6d
D-Sketon <2055272094@qq.com>
2023-12-30 11:55:30 +0800

    feat: 适配v7

--- scripts/injector/codeBock.js
+++ scripts/injector/codeBock.js
@@ -1,10 +1,8 @@
 const js = hexo.extend.helper.get('js').bind(hexo);
 
-if (!hexo.config._theme_config || hexo.config._theme_config.code_beautify.enable) {
-  if (hexo.config.highlight.enable) {
-    hexo.extend.injector.register('body_end', () => {
-      return js('/js/insertHighlight.js');
-    }, 'default');
-  }
+if (hexo.config.highlight.enable || hexo.config.syntax_highlighter === 'highlight.js') {
+  hexo.extend.injector.register('body_end', () => {
+    return js('/js/insertHighlight.js');
+  }, 'default');
 }
 
