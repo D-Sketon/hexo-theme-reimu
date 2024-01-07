e9b773616816cb0183dfea4ee1b11f317f62f2cf
手瓜一十雪 <nanaeonn@outlook.com>
2024-01-07 21:23:37 +0800

    feat:新增内置ServiceWorker支持

---
+++ scripts/generator/servicework.js
@@ -0,0 +1,12 @@
+const fs = require('hexo-fs')
+let timeVersion = Date.now()
+
+hexo.extend.generator.register('ServiceWoker', function(locals){
+    let filePath = 'themes/reimu/source/js/sw.js'
+    let content = fs.readFileSync(filePath)
+    content = 'const VERSION = "'+ timeVersion +'";\n' + content;
+    return {
+      path: "sw.js",
+      data: content,
+    }
+})
