41942ca3ce355365cbcf6e20f665988699a3e3c8
D-Sketon <2055272094@qq.com>
2024-01-12 13:10:22 +0800

    refactor: vendor

---
+++ scripts/helper/vendorCdn.js
@@ -0,0 +1,16 @@
+hexo.extend.helper.register('vendorCdn', (content) => {
+  if (!Array.isArray(content)) {
+    content = [content]
+  }
+  return content.map(item => {
+    const contentToken = item.split('|')
+    if (contentToken.length !== 2) {
+      throw new Error("invalid vendor")
+    }
+    const cdn = contentToken[0]
+    if (!hexo.theme.config.vendor[cdn]) {
+      throw new Error("invalid cdn")
+    }
+    return hexo.theme.config.vendor[cdn] + contentToken[1]
+  })
+})
