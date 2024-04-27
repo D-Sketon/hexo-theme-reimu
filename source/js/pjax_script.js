8f8a460f1ca7bd1069b18c7872189201b569bd90
D-Sketon <2055272094@qq.com>
2024-04-27 13:39:26 +0800

    fix: hexo-blog-decrypt TOC适配

--- source/js/pjax_script.js
+++ source/js/pjax_script.js
@@ -204,7 +204,7 @@ var scrollIntoViewAndWait = (element) => {
     $('body').removeClass('mobile-nav-on');
   });
 
-  (function () {
+  function tocInit() {
     const navItems = getComputedStyle(document.getElementById('sidebar')).display === 'block' ?
       $('#sidebar .sidebar-toc-wrapper li') :
       $('#mobile-nav .sidebar-toc-wrapper li');
@@ -302,5 +302,8 @@ var scrollIntoViewAndWait = (element) => {
     sections.each(function () {
       $(this).length && observer.observe($(this)[0])
     })
-  })();
+  };
+
+  window.addEventListener('hexo-blog-decrypt', tocInit);
+  tocInit();
 })(jQuery);
