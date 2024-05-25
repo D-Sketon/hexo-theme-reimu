28183e77bf46291cd4c84081f50f4f96b1ce6303
D-Sketon <2055272094@qq.com>
2024-05-25 15:56:17 +0800

    perf: 字体优化

--- scripts/helper/vendorFont.js
+++ scripts/helper/vendorFont.js
@@ -1,25 +1,26 @@
 const { htmlTag } = require('hexo-util')
 hexo.extend.helper.register('vendorFont', () => {
   const fontDisplay = '&display=swap'
-  const fontSubset = '&subset=latin,latin-ext'
   const fontStyles = ':400,400italic,700,700italic'
   const fontHost = 'https://fonts.googleapis.com'
 
-  let fontFamilies = ['Mulish', 'Noto Serif SC', 'Ubuntu Mono'].map(item => {
-    return item + fontStyles
+  let fontFamilies = ['Mulish', 'Noto Serif SC', 'Ubuntu Mono']
+    .map(item => item + fontStyles)
+    .filter(item => item !== '')
+    .join('|')
+
+  return htmlTag('link', {
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossorigin: true,
+  }) + htmlTag('link', {
+    rel: 'preload',
+    as: 'style',
+    href: `${fontHost}/css?family=${fontFamilies.concat(fontDisplay)}`,
+  }) + htmlTag('link', {
+    rel: 'stylesheet',
+    href: `${fontHost}/css?family=${fontFamilies.concat(fontDisplay)}`,
+    media: 'print',
+    onload: "this.media='all'"
   })
-
-  fontFamilies = fontFamilies.filter(item => item !== '')
-  fontFamilies = [...new Set(fontFamilies)]
-  fontFamilies = fontFamilies.join('|')
-
-  // Merge extra parameters to the final processed font string
-  return fontFamilies
-    ? htmlTag('link', {
-      rel: 'preload',
-      href: `${fontHost}/css?family=${fontFamilies.concat(fontDisplay, fontSubset)}`,
-      as: "style",
-      onload: "this.onload=null;this.rel='stylesheet'"
-    })
-    : ''
 })
