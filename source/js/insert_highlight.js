47aef12c687560936a55cacd2d004afe80ac5ed9
D-Sketon <2055272094@qq.com>
2024-12-07 11:30:53 +0800

    feat: code_block expand

--- source/js/insert_highlight.js
+++ source/js/insert_highlight.js
@@ -3,7 +3,7 @@
     const parent = element.parentNode;
     if (!parent.classList.contains("gutter")) {
       const div = document.createElement("div");
-      div.classList.add("code-area");
+      div.className = "code-area";
       parent.insertBefore(div, element);
       parent.removeChild(element);
       div.appendChild(element);
@@ -21,20 +21,28 @@
       <div class="icon-chevron-down code-expand"></div>
     </div>
   </div>`;
+  const reimuConfig = window.REIMU_CONFIG?.code_block || {};
+  const expandThreshold = reimuConfig.expand;
+
   _$$("figure.highlight").forEach((element) => {
     if (!element.querySelector(".code-figcaption")) {
       element.insertAdjacentHTML("afterbegin", codeFigcaption);
     }
+    if (expandThreshold !== undefined) {
+      if (
+        expandThreshold === false ||
+        (typeof expandThreshold === "number" &&
+          element.querySelectorAll("td.code .line").length > expandThreshold)
+      ) {
+        element.classList.add("code-closed");
+      }
+    }
   });
   // 代码收缩
   _$$(".code-expand").forEach((element) => {
-    element.off("click").on("click", function () {
+    element.off("click").on("click", () => {
       const figure = element.closest("figure");
-      if (figure.classList.contains("code-closed")) {
-        figure.classList.remove("code-closed");
-      } else {
-        figure.classList.add("code-closed");
-      }
+      figure.classList.toggle("code-closed");
     });
   });
 
@@ -62,29 +70,36 @@
     return;
   }
 
+  const tips = window.REIMU_CONFIG?.clipboard_tips || {};
+
   // 代码复制
   const clipboard = new ClipboardJS(".code-copy", {
     text: (trigger) => {
       const selection = window.getSelection();
       const range = document.createRange();
 
-      range.selectNodeContents(trigger.parentNode.parentNode.nextElementSibling.querySelector("td.code"));
+      range.selectNodeContents(
+        trigger.parentNode.parentNode.nextElementSibling.querySelector(
+          "td.code"
+        )
+      );
       selection.removeAllRanges();
       selection.addRange(range);
 
       let selectedText = selection.toString();
-      if (window.REIMU_CONFIG.clipboard_tips.copyright?.enable) {
-        if (selectedText.length >= window.REIMU_CONFIG.clipboard_tips.copyright?.count) {
-          selectedText = selectedText + "\n\n" + window.REIMU_CONFIG.clipboard_tips.copyright?.content ?? '';
-        }
+      if (
+        tips.copyright?.enable &&
+        selectedText.length >= tips.copyright?.count
+      ) {
+        selectedText = selectedText + "\n\n" + tips.copyright?.content ?? "";
       }
       return selectedText;
     },
   });
-  clipboard.on("success", function (e) {
+  clipboard.on("success", (e) => {
     e.trigger.classList.add("icon-check");
     e.trigger.classList.remove("icon-copy");
-    _$("#copy-tooltip").innerText = window.REIMU_CONFIG.clipboard_tips.success;
+    _$("#copy-tooltip").innerText = tips.success;
     _$("#copy-tooltip").style.opacity = 1;
     setTimeout(() => {
       _$("#copy-tooltip").style.opacity = 0;
@@ -94,10 +109,10 @@
     e.clearSelection();
   });
 
-  clipboard.on("error", function (e) {
+  clipboard.on("error", (e) => {
     e.trigger.classList.add("icon-times");
     e.trigger.classList.remove("icon-copy");
-    _$("#copy-tooltip").innerText = window.REIMU_CONFIG.clipboard_tips.fail;
+    _$("#copy-tooltip").innerText = tips.fail;
     _$("#copy-tooltip").style.opacity = 1;
     setTimeout(() => {
       _$("#copy-tooltip").style.opacity = 0;
@@ -106,10 +121,14 @@
     }, 1000);
   });
 
-  // clear clipboard when pjax:send
+  // Clean up on PJAX
   if (window.Pjax) {
-    window.addEventListener("pjax:send", () => {
-      clipboard.destroy();
-    }, { once: true });
+    window.addEventListener(
+      "pjax:send",
+      () => {
+        clipboard.destroy();
+      },
+      { once: true }
+    );
   }
 })();
