1b59111c233a23a395174ac60eb2f6e43f42ad2a
D-Sketon <2055272094@qq.com>
2025-01-04 13:06:47 +0800

    feat: enhance article copyright display with icons

--- scripts/helper/articleCopyright.js
+++ scripts/helper/articleCopyright.js
@@ -1,14 +1,14 @@
 hexo.extend.helper.register("articleCopyright", (post) => {
-  const authorDom = `<p><strong>本文作者：</strong>${hexo.config.author} @ ${hexo.config.title}</p>`;
-  const linkDom = `<p><strong>本文链接：</strong><a href="${post.permalink}">${post.permalink}</a></p>`;
-  const titleDom = `<p><strong>本文标题：</strong>${post.title}</p>`;
-  const dateDom = `<p><strong>本文发布时间：</strong>${post.date?.format(
+  const authorDom = `<p><strong><span class="icon-user icon"></span>本文作者：</strong>${hexo.config.author} @ ${hexo.config.title}</p>`;
+  const linkDom = `<p><strong><span class="icon-link icon"></span>本文链接：</strong><a href="${post.permalink}">${post.permalink}</a></p>`;
+  const titleDom = `<p><strong><span class="icon-pencil icon"></span>本文标题：</strong>「${post.title}」</p>`;
+  const dateDom = `<p><strong><span class="icon-calendar icon"></span>本文发布时间：</strong>${post.date?.format(
     "YYYY-MM-DD HH:mm:ss"
   ) ?? '未知(ﾟ⊿ﾟ)ﾂ'}</p>`;
-  const updatedDom = `<p><strong>本文修改时间：</strong>${post.updated?.format(
+  const updatedDom = `<p><strong><span class="icon-calendar icon"></span>本文修改时间：</strong>${post.updated?.format(
     "YYYY-MM-DD HH:mm:ss"
   ) ?? '未知(ﾟ⊿ﾟ)ﾂ'}</p>`;
-  const licenseDom = `<p><strong>本文版权：</strong>本博客所有文章除特别声明外，均采用 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh" rel="noopener external nofollow noreferrer" target="_blank"><span class="icon-creative-commons"></span>BY-NC-SA</a> 许可协议。转载请注明出处！</p>`;
+  const licenseDom = `<p><strong><span class="icon-copyright icon"></span>本文版权：</strong>本博客所有文章除特别声明外，均采用 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh" rel="noopener external nofollow noreferrer" target="_blank"><span class="icon-creative-commons"></span>BY-NC-SA</a> 许可协议。转载请注明出处！</p>`;
   const { author, link, title, date, updated, license } =
     hexo.theme.config.article_copyright.content;
   return `<blockquote class="article-copyright">
