57285e7724c62632ce38099d93b5d9b6cbada8f8
D-Sketon <2055272094@qq.com>
2025-02-22 20:49:25 +0800

    feat: article copyright support i18n

--- scripts/helper/articleCopyright.js
+++ scripts/helper/articleCopyright.js
@@ -1,6 +1,8 @@
 const { full_url_for } = require("hexo-util");
 
-hexo.extend.helper.register("articleCopyright", (post) => {
+const CC = '<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh" rel="noopener external nofollow noreferrer" target="_blank"><span class="icon-creative-commons"></span>BY-NC-SA</a>';
+
+hexo.extend.helper.register("articleCopyright", function (post) {
   const siteLang = hexo.config.language;
   const lang = post.lang;
   let permalink;
@@ -14,16 +16,16 @@ hexo.extend.helper.register("articleCopyright", (post) => {
   } else {
     permalink = post.permalink;
   }
-  const authorDom = `<p><strong><span class="icon-user icon"></span>本文作者：</strong>${hexo.config.author} @ ${hexo.config.title}</p>`;
-  const linkDom = `<p><strong><span class="icon-link icon"></span>本文链接：</strong><a href="${permalink}">${permalink}</a></p>`;
-  const titleDom = `<p><strong><span class="icon-pencil icon"></span>本文标题：</strong>「${post.title}」</p>`;
-  const dateDom = `<p><strong><span class="icon-calendar icon"></span>本文发布时间：</strong>${post.date?.format(
+  const authorDom = `<p><strong><span class="icon-user icon"></span>${this.__('article_copyright.author')}</strong>${hexo.config.author} @ ${hexo.config.title}</p>`;
+  const linkDom = `<p><strong><span class="icon-link icon"></span>${this.__('article_copyright.permalink')}</strong><a href="${permalink}">${permalink}</a></p>`;
+  const titleDom = `<p><strong><span class="icon-pencil icon"></span>${this.__('article_copyright.title')}</strong>「${post.title}」</p>`;
+  const dateDom = `<p><strong><span class="icon-calendar icon"></span>${this.__('article_copyright.date')}</strong>${post.date?.format(
     "YYYY-MM-DD HH:mm:ss"
   ) ?? '未知(ﾟ⊿ﾟ)ﾂ'}</p>`;
-  const updatedDom = `<p><strong><span class="icon-calendar icon"></span>本文修改时间：</strong>${post.updated?.format(
+  const updatedDom = `<p><strong><span class="icon-calendar icon"></span>${this.__('article_copyright.updated')}</strong>${post.updated?.format(
     "YYYY-MM-DD HH:mm:ss"
   ) ?? '未知(ﾟ⊿ﾟ)ﾂ'}</p>`;
-  const licenseDom = `<p><strong><span class="icon-copyright icon"></span>本文版权：</strong>本博客所有文章除特别声明外，均采用 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh" rel="noopener external nofollow noreferrer" target="_blank"><span class="icon-creative-commons"></span>BY-NC-SA</a> 许可协议。转载请注明出处！</p>`;
+  const licenseDom = `<p><strong><span class="icon-copyright icon"></span>${this.__('article_copyright.license')}</strong>${this._p('article_copyright.license_content', CC)}</p>`;
   const { author, link, title, date, updated, license } =
     hexo.theme.config.article_copyright.content;
   return `<blockquote class="article-copyright">
