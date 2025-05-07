a20b0e56d129d3f00451b36de837ac269f6cf610
D-Sketon <2055272094@qq.com>
2025-05-07 23:58:55 +0800

    feat: add sri support for css

--- scripts/helper/asyncCss.js
+++ scripts/helper/asyncCss.js
@@ -1,3 +1,4 @@
+const { htmlTag } = require("hexo-util");
 hexo.extend.helper.register("asyncCss", (content) => {
   if (!Array.isArray(content)) {
     content = [content];
@@ -5,10 +6,21 @@ hexo.extend.helper.register("asyncCss", (content) => {
 
   return content
     .map((item) => {
-      if (!item.endsWith(".css")) {
-        item += ".css";
+      if (typeof item === "string") {
+        let path = item;
+        if (!path.endsWith(".css")) {
+          path += ".css";
+        }
+        return `<link rel="preload" href="${item}" as="style" onload="this.onload=null;this.rel='stylesheet'">`;
+      } else {
+        if (!item.href.endsWith(".css")) item.href += ".css";
+        return htmlTag("link", {
+          rel: "preload",
+          as: "style",
+          onload: "this.onload=null;this.rel='stylesheet'",
+          ...item,
+        });
       }
-      return `<link rel="preload" href="${item}" as="style" onload="this.onload=null;this.rel='stylesheet'">`;
     })
     .join("\n");
 });
